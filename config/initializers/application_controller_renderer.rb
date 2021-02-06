ef7be41e14d364e0662cde599f09e9fd43cdd86e
higashitetuya <azmax_0817@yahoo.co.jp>
2021-02-06 14:41:28 +0000

    [FINISH]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '56b9a7a2b353264b9f590df219c63d8b3dc53a938032b2b120650c718ba879c0bb0131dec4a6ae3c88643c955f3b6fff607a2394f62a97e22fead063b3ff47ed'
